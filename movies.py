d5bc3b31bad3d2e72bb8d7392badacffa120583f
nomifoster <nomi.foster@gmail.com>
2015-08-21 10:44:20 -0400

    Movies Playtime
    playing with directories using strings

---
+++ movies.py
@@ -0,0 +1,18 @@
+movie_titles = ['Amelie','The Mummy','How To Train Your Dragon','Howls Moving Castle','Empire Records']
+parental_rating = ['R','PG13','PG','PG','PG13']
+bechdel_rating = ['3','1','3','3','3']
+imdb_rating = ['8.4','7','8.2','8.2','6.7']
+genre = ['Romance/Comedy','Fantasy/Action','Fantasy/Drama','Fantasy/Drama','Coming of Age/Cult']
+
+
+Amelie = movie_titles[0],parental_rating[0],bechdel_rating[0],imdb_rating[0],genre[0]
+The_Mummy = movie_titles[1],parental_rating[1],bechdel_rating[1],imdb_rating[1],genre[1]
+How_To_Train_Your_Dragon = movie_titles[2],parental_rating[2],bechdel_rating[2],imdb_rating[2],genre[2]
+Howls_Moving_Castle = movie_titles[3],parental_rating[3],bechdel_rating[3],imdb_rating[3],genre[3]
+Empire_Records = movie_titles[4],parental_rating[4],bechdel_rating[4],imdb_rating[4],genre[4]
+
+print Amelie
+print The_Mummy
+print How_To_Train_Your_Dragon
+print Howls_Moving_Castle
+print Empire_Records
