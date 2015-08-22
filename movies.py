579f3ac7df2cca36082577f29b4dce5ced198ba4
nomifoster <nomi.foster@gmail.com>
2015-08-22 10:32:04 -0400

    Update movies.py

--- movies.py
+++ movies.py
@@ -4,15 +4,8 @@ bechdel_rating = ['3','1','3','3','3']
 imdb_rating = ['8.4','7','8.2','8.2','6.7']
 genre = ['Romance/Comedy','Fantasy/Action','Fantasy/Drama','Fantasy/Drama','Coming of Age/Cult']
 
+n = 0
 
-Amelie = movie_titles[0],parental_rating[0],bechdel_rating[0],imdb_rating[0],genre[0]
-The_Mummy = movie_titles[1],parental_rating[1],bechdel_rating[1],imdb_rating[1],genre[1]
-How_To_Train_Your_Dragon = movie_titles[2],parental_rating[2],bechdel_rating[2],imdb_rating[2],genre[2]
-Howls_Moving_Castle = movie_titles[3],parental_rating[3],bechdel_rating[3],imdb_rating[3],genre[3]
-Empire_Records = movie_titles[4],parental_rating[4],bechdel_rating[4],imdb_rating[4],genre[4]
-
-print Amelie
-print The_Mummy
-print How_To_Train_Your_Dragon
-print Howls_Moving_Castle
-print Empire_Records
+for movie in movie_titles:
+	print movie_titles[n] + ", " + parental_rating[n] + "," + imdb_rating[n] + ", " + bechdel_rating[n] + ", " + genre[n]
+	n = n + 1
